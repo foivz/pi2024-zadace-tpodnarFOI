01586c93318ff338c452e651541945af1ffa473f
Tin Podnar <tpodnar22@student.foi.hr>
2024-06-02 17:58:06 +0200

    Implementacija forme za prijavu. Dodan FrmLogin preko kojeg se vrši prijava, kasnije se povezuje s bazom.

---
+++ Software/VŽBUS+/VŽBUS+/FrmLogin.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace VŽBUS_
+{
+    partial class FrmLogin
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.labelUsername = new System.Windows.Forms.Label();
+            this.textBoxUsername = new System.Windows.Forms.TextBox();
+            this.labelPassword = new System.Windows.Forms.Label();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.buttonLogin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelUsername
+            // 
+            this.labelUsername.AutoSize = true;
+            this.labelUsername.Location = new System.Drawing.Point(16, 18);
+            this.labelUsername.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelUsername.Name = "labelUsername";
+            this.labelUsername.Size = new System.Drawing.Size(100, 16);
+            this.labelUsername.TabIndex = 0;
+            this.labelUsername.Text = "Korisničko ime: ";
+            // 
+            // textBoxUsername
+            // 
+            this.textBoxUsername.CausesValidation = false;
+            this.textBoxUsername.Location = new System.Drawing.Point(136, 15);
+            this.textBoxUsername.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxUsername.Name = "textBoxUsername";
+            this.textBoxUsername.Size = new System.Drawing.Size(225, 22);
+            this.textBoxUsername.TabIndex = 1;
+            this.textBoxUsername.TextChanged += new System.EventHandler(this.textBoxUsername_TextChanged);
+            // 
+            // labelPassword
+            // 
+            this.labelPassword.AutoSize = true;
+            this.labelPassword.Location = new System.Drawing.Point(16, 50);
+            this.labelPassword.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelPassword.Name = "labelPassword";
+            this.labelPassword.Size = new System.Drawing.Size(59, 16);
+            this.labelPassword.TabIndex = 2;
+            this.labelPassword.Text = "Lozinka: ";
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.CausesValidation = false;
+            this.textBoxPassword.Location = new System.Drawing.Point(136, 47);
+            this.textBoxPassword.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(225, 22);
+            this.textBoxPassword.TabIndex = 3;
+            this.textBoxPassword.UseSystemPasswordChar = true;
+            this.textBoxPassword.TextChanged += new System.EventHandler(this.textBoxPassword_TextChanged);
+            // 
+            // buttonLogin
+            // 
+            this.buttonLogin.CausesValidation = false;
+            this.buttonLogin.Location = new System.Drawing.Point(136, 79);
+            this.buttonLogin.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.Size = new System.Drawing.Size(100, 28);
+            this.buttonLogin.TabIndex = 4;
+            this.buttonLogin.Text = "Prijava";
+            this.buttonLogin.UseVisualStyleBackColor = true;
+            // 
+            // FrmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CausesValidation = false;
+            this.ClientSize = new System.Drawing.Size(379, 124);
+            this.Controls.Add(this.buttonLogin);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.labelPassword);
+            this.Controls.Add(this.textBoxUsername);
+            this.Controls.Add(this.labelUsername);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Prijava u VŽBUS+";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelUsername;
+        private System.Windows.Forms.TextBox textBoxUsername;
+        private System.Windows.Forms.Label labelPassword;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.Button buttonLogin;
+    }
+}
---
+++ Software/VŽBUS+/VŽBUS+/FrmLogin.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VŽBUS_
+{
+    public partial class FrmLogin : Form
+    {
+        string username = "putnik";
+        string password = "test";
+
+        public FrmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void textBoxUsername_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxPassword_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+            if (textBoxUsername.Text == "")
+            {
+                MessageBox.Show("Korisničko ime nije uneseno!", "Problem",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (textBoxPassword.Text == "")
+            {
+                MessageBox.Show("Lozinka nije unesena!", "Problem", MessageBoxButtons.OK,
+                MessageBoxIcon.Error);
+            }
+            else
+            {
+                if (textBoxUsername.Text == username && textBoxPassword.Text == password)
+                {
+                    MessageBox.Show("Dobrodošli!", "Prijavljeni ste",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Krivi podaci!", "Problem", MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+                }
+            }
+        }
+    }
+}
